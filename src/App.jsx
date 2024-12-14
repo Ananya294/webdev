cfc04d380d3c6a46a76d82b65f6571afe91b9ad8
Ananya294 <singhananyaas294@gmail.com>
2024-12-14 18:59:29 +0530

    state lifting

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,18 @@ import './App.css'
 import Counter from './components/Counter'
 import Card from './components/Card'
 import Button from './components/Button'
+import Card1 from './components/Card1'
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleClick(){
-    setCount(count+1);
-  }
-
+  const [name, setName] = useState('');
 
   return (
     <>
       <div>
-      <Button handleClick={handleClick} text="click me" >
-        <h1>{count}</h1>
-      </Button>
+        <Card1 title = "card1" name={name} setName={setName}/>
+        <Card1 title = "card2" name={name} setName={setName}/>
+        {/* <p>inside parent component {name} </p> */}
       </div>
     </>
   )
---
+++ src/components/Card1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card1 = (props) => {
+  return (
+    <div>
+        <input type="text" onChange={(e)=> props.setName(e.target.value)} />
+        <p>Name state variable value inside {props.title}: {props.name}</p>
+    </div>
+  )
+}
+
+export default Card1
