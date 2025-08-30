1b6659ab2036de58af96dae58fc09d8f2d99bb53
Ananya294 <singhananyaas294@gmail.com>
2025-08-30 15:42:21 +0530

    learning use context 1

---
+++ context_api/context_api/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>usercontext demo</h1>
+      <Login/>
+      <Profile/>
+
+    </UserContextProvider>
+  )
+}
+
+export default App
